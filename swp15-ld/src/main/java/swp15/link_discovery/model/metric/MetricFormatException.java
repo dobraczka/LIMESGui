30c6cd8553161147c4d8dcb23372189dca683fee
hicks <Sascha_Uni@Katharina-Zen>
2015-05-10 18:18:30 +0200

    Added JavaDoc Comments

--- swp15-ld/src/main/java/swp15/link_discovery/model/metric/MetricFormatException.java
+++ swp15-ld/src/main/java/swp15/link_discovery/model/metric/MetricFormatException.java
@@ -1,9 +1,42 @@
 package swp15.link_discovery.model.metric;
 
-public class MetricFormatException extends RuntimeException
-{
-	private static final long	serialVersionUID	= -1394278085750921482L;
-	public MetricFormatException() {super();}
-	public MetricFormatException(String s) {super(s);}
-	public MetricFormatException(String s,Throwable cause) {super(s,cause);}
+/**
+ * Exception if Metric is not in the right Format
+ *
+ */
+public class MetricFormatException extends RuntimeException {
+
+	/**
+	 * serialVersion
+	 */
+	private static final long serialVersionUID = 7711263588635597763L;
+
+	/**
+	 * Constructor
+	 */
+	public MetricFormatException() {
+		super();
+	}
+
+	/**
+	 * Constructor with Message
+	 * 
+	 * @param s
+	 *            Message
+	 */
+	public MetricFormatException(String s) {
+		super(s);
+	}
+
+	/**
+	 * Constructor with Message and Cause of Exception
+	 * 
+	 * @param s
+	 *            Message
+	 * @param cause
+	 *            Cause of Exception
+	 */
+	public MetricFormatException(String s, Throwable cause) {
+		super(s, cause);
+	}
 }
--- swp15-ld/src/main/java/swp15/link_discovery/model/metric/MetricParser.java
+++ swp15-ld/src/main/java/swp15/link_discovery/model/metric/MetricParser.java
@@ -6,95 +6,147 @@ import java.util.List;
 
 import swp15.link_discovery.model.metric.Property.Origin;
 
-public class MetricParser
-{
-	protected static String[] splitFunc(String s)
-	{
-		if(!s.contains("(")) {return new String[] {s};}
+/**
+ * Parses Metric to Expression for the Config Reader
+ *
+ */
+public class MetricParser {
+	/**
+	 * Splits the String
+	 * 
+	 * @param s
+	 *            String to Split
+	 * @return String list
+	 */
+	protected static String[] splitFunc(String s) {
+		if (!s.contains("(")) {
+			return new String[] { s };
+		}
 		List<String> tokenList = new LinkedList<String>();
 		int i = s.indexOf("(");
-		tokenList.add(s.substring(0,i));
-		s = s.substring(i,s.length());
+		tokenList.add(s.substring(0, i));
+		s = s.substring(i, s.length());
 		int depth = 0;
 		StringBuilder sb = new StringBuilder();
-		for(char c : s.toCharArray())
-		{
-			switch(c)
-			{
-				case('('): if(depth>0) {sb.append(c);} depth++; break;
-				case(')'): if(depth>1) {sb.append(c);} depth--; break;
-				case(','):
-					if(depth>1) {sb.append(c);} else {tokenList.add(sb.toString());sb = new StringBuilder();}
+		for (char c : s.toCharArray()) {
+			switch (c) {
+			case ('('):
+				if (depth > 0) {
+					sb.append(c);
+				}
+				depth++;
 				break;
-				default: sb.append(c);
+			case (')'):
+				if (depth > 1) {
+					sb.append(c);
+				}
+				depth--;
+				break;
+			case (','):
+				if (depth > 1) {
+					sb.append(c);
+				} else {
+					tokenList.add(sb.toString());
+					sb = new StringBuilder();
+				}
+				break;
+			default:
+				sb.append(c);
 			}
 		}
 		tokenList.add(sb.toString());
 		return tokenList.toArray(new String[0]);
 	}
 
-	protected static String setParametersFromString(Node node, String s, int pos)
-	{
-		if(node.hasFrontParameters())
-		{
+	/**
+	 * Sets the parameters of a Node from StringExpression
+	 * 
+	 * @param node
+	 *            Node to be configurated
+	 * @param s
+	 *            Expression
+	 * @param pos
+	 *            Position of Node
+	 * @return Node String
+	 */
+	protected static String setParametersFromString(Node node, String s, int pos) {
+		if (node.hasFrontParameters()) {
 			int i = s.indexOf("*");
-			if(i==-1) return s;
-			Double d = Double.valueOf(s.substring(0,i));
-			s = s.substring(i+1);
-			switch(pos)
-			{
-				case(0):node.param1=d;break;
-				case(1):node.param2=d;
+			if (i == -1)
+				return s;
+			Double d = Double.valueOf(s.substring(0, i));
+			s = s.substring(i + 1);
+			switch (pos) {
+			case (0):
+				node.param1 = d;
+				break;
+			case (1):
+				node.param2 = d;
 			}
-		}
-		else
-		{
+		} else {
 			int i = s.lastIndexOf("|");
-			if(i==-1||i<s.lastIndexOf(")")) {return s;}
-			Double d = Double.valueOf(s.substring(i+1));
-			s = s.substring(0,i);
-			switch(pos)
-			{
-				case(0):node.param1=d;break;
-				case(1):node.param2=d;
+			if (i == -1 || i < s.lastIndexOf(")")) {
+				return s;
+			}
+			Double d = Double.valueOf(s.substring(i + 1));
+			s = s.substring(0, i);
+			switch (pos) {
+			case (0):
+				node.param1 = d;
+				break;
+			case (1):
+				node.param2 = d;
 			}
 		}
 		return s;
 	}
 
-	protected static Node parsePart(Node parent, String s, String sourceVar, int pos) throws MetricFormatException
-	{
+	protected static Node parsePart(Node parent, String s, String sourceVar,
+			int pos) throws MetricFormatException {
 		String[] tokens = splitFunc(s);
 		String id = tokens[0].trim();
-		if(id.contains("*")) 
-		{
+		if (id.contains("*")) {
 			String[] parts = id.split("\\*");
 			double d = Double.parseDouble(parts[0]);
-			if(parent.param1==null) {parent.param1=d;} else {parent.param2=d;}
+			if (parent.param1 == null) {
+				parent.param1 = d;
+			} else {
+				parent.param2 = d;
+			}
 			id = parts[1];
 		}
-		if(tokens.length==1) return new Property(id,id.startsWith(sourceVar)?Origin.SOURCE:Origin.TARGET);
+		if (tokens.length == 1)
+			return new Property(id, id.startsWith(sourceVar) ? Origin.SOURCE
+					: Origin.TARGET);
 
 		Node node = Node.createNode(id);
-		s=setParametersFromString(parent,s,pos);
+		s = setParametersFromString(parent, s, pos);
 		tokens = splitFunc(s);
-		int i=0;
-		for(String argument: Arrays.copyOfRange(tokens,1,tokens.length))
-		{
-			if(!node.addChild(parsePart(node,argument,sourceVar,i))) {throw new MetricFormatException("Could not add child \""+argument+'"');}
+		int i = 0;
+		for (String argument : Arrays.copyOfRange(tokens, 1, tokens.length)) {
+			if (!node.addChild(parsePart(node, argument, sourceVar, i))) {
+				throw new MetricFormatException("Could not add child \""
+						+ argument + '"');
+			}
 			i++;
 		}
 		return node;
 	}
-	public static Output parse(String s, String sourceVar) throws MetricFormatException
-	{
-		if(s.isEmpty()) throw new MetricFormatException();
+
+	public static Output parse(String s, String sourceVar)
+			throws MetricFormatException {
+		if (s.isEmpty())
+			throw new MetricFormatException();
 		Output output = new Output();
-		s = setParametersFromString(output,s,0);
-		output.param1=null;
-		output.param2=null;
-		try{output.addChild(parsePart(output, s,sourceVar,0));}
-		catch(MetricFormatException e) {throw new MetricFormatException("Error parsing metric expression \""+s+"\".",e);}
+		s = setParametersFromString(output, s, 0);
+		output.param1 = null;
+		output.param2 = null;
+		try {
+			output.addChild(parsePart(output, s, sourceVar, 0));
+		} catch (MetricFormatException e) {
+			throw new MetricFormatException(
+					"Error parsing metric expression \"" + s + "\".", e);
+		}
 		return output;
 	}
 }
