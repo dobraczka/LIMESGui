7808d4d5318c7ef7615093ff99780ed9bcc4f67d
hicks <Sascha_Uni@Katharina-Zen>
2015-05-10 18:09:15 +0200

    Added Comments in Measure and Node

--- swp15-ld/src/main/java/swp15/link_discovery/model/metric/Measure.java
+++ swp15-ld/src/main/java/swp15/link_discovery/model/metric/Measure.java
@@ -7,45 +7,98 @@ import java.util.Set;
 
 import swp15.link_discovery.model.metric.Property.Origin;
 
-public class Measure extends Node
-{
-	public Measure(String id) {super(id);}
-	public static final Set<String> identifiers = Collections.unmodifiableSet(new HashSet<String>(Arrays.asList(
-			new String[] {"cosine","euclidean","jaccard","levenshtein","overlap","trigrams" })));
+/**
+ * To Measure similarities
+ *
+ */
+public class Measure extends Node {
+	/**
+	 * Constructor
+	 * 
+	 * @param id
+	 *            Id of the Node
+	 */
+	public Measure(String id) {
+		super(id);
+	}
 
-	@Override public Set<String> identifiers()	{return identifiers;}
-	@Override public byte getMaxChilds() {return 2;}
+	/**
+	 * List of the possible Measurements
+	 */
+	public static final Set<String> identifiers = Collections
+			.unmodifiableSet(new HashSet<String>(Arrays.asList(new String[] {
+					"cosine", "euclidean", "jaccard", "levenshtein", "overlap",
+					"trigrams" })));
 
-	static public final Set<Class<? extends Node>> validChildClasses =
-			Collections.<Class<? extends Node>>singleton(Property.class);
-	@Override public Set<Class<? extends Node>> validChildClasses() {return validChildClasses;}
+	/**
+	 * Returns the Measurement identifiers
+	 * 
+	 * @return measurement identifiers
+	 */
+	@Override
+	public Set<String> identifiers() {
+		return identifiers;
+	}
 
+	/**
+	 * Returns the Number of maximal Children of the Node
+	 * 
+	 * @return Number of Children
+	 */
+	@Override
+	public byte getMaxChilds() {
+		return 2;
+	}
 
-	@Override public boolean acceptsChild(Node node)
-	{
-		synchronized(this)
-		{
-			synchronized (node)
-			{
-				return super.acceptsChild(node)&&
-						(getChilds().isEmpty()
-								||((Property)getChilds().iterator().next()).origin!=((Property)node).origin);
+	/**
+	 * Set of valid Child-Classes
+	 */
+	static public final Set<Class<? extends Node>> validChildClasses = Collections
+			.<Class<? extends Node>> singleton(Property.class);
+
+	/**
+	 * Returns the valid child-classes
+	 * 
+	 * @return valid child classes
+	 */
+	@Override
+	public Set<Class<? extends Node>> validChildClasses() {
+		return validChildClasses;
+	}
+
+	/**
+	 * Returns if node accepts child
+	 * 
+	 * @return true if Node accepts Child
+	 */
+	@Override
+	public boolean acceptsChild(Node node) {
+		synchronized (this) {
+			synchronized (node) {
+				return super.acceptsChild(node)
+						&& (getChilds().isEmpty() || ((Property) getChilds()
+								.iterator().next()).origin != ((Property) node).origin);
 			}
 		}
 	}
 
+	/**
+	 * Returns the Acceptance of linkedChild
+	 * 
+	 * @return Acceptance of linked Child
+	 */
 	@Override
-	public Acceptance acceptsChildWithReason(Node node)
-	{
-		synchronized(this)
-		{
-			synchronized(node)
-			{
+	public Acceptance acceptsChildWithReason(Node node) {
+		synchronized (this) {
+			synchronized (node) {
 				Acceptance acceptance = super.acceptsChildWithReason(node);
-				if(acceptance!=Acceptance.OK) {return acceptance;}
-				if(!acceptsChild(node))
-				{
-					if((((Property)getChilds().iterator().next()).origin==Origin.SOURCE)) {return Acceptance.TARGET_PROPERTY_EXPECTED;}
+				if (acceptance != Acceptance.OK) {
+					return acceptance;
+				}
+				if (!acceptsChild(node)) {
+					if ((((Property) getChilds().iterator().next()).origin == Origin.SOURCE)) {
+						return Acceptance.TARGET_PROPERTY_EXPECTED;
+					}
 					return Acceptance.SOURCE_PROPERTY_EXPECTED;
 				}
 				return Acceptance.OK;
--- swp15-ld/src/main/java/swp15/link_discovery/model/metric/Node.java
+++ swp15-ld/src/main/java/swp15/link_discovery/model/metric/Node.java
@@ -5,94 +5,218 @@ import java.util.List;
 import java.util.Set;
 import java.util.Vector;
 
-public abstract class Node
-{
+public abstract class Node {
+	/**
+	 * 
+	 */
 	public Double param1 = null;
 	public Double param2 = null;
+
+	/**
+	 * Node Id
+	 */
 	public final String id;
+
+	/**
+	 * Returns the valid identifiers of the Node
+	 * 
+	 * @return valid identifiers
+	 */
 	public abstract Set<String> identifiers();
+
+	/**
+	 * Color of node
+	 */
 	protected static int colors = 0;
+	/**
+	 * other Color of the node
+	 */
 	protected int color;
-	public byte getMaxChilds() {return 0;}
+
+	/**
+	 * Returns the maxmimum number of children of the node
+	 * 
+	 * @return number of children
+	 */
+	public byte getMaxChilds() {
+		return 0;
+	}
+
+	/**
+	 * Valid accepted Child classes of the node
+	 * 
+	 * @return valid accepted classes
+	 */
 	public abstract Set<Class<? extends Node>> validChildClasses();
-	public final boolean isValidParentOf(Node node) {return validChildClasses().contains(node.getClass());}
+
+	/**
+	 * Returns if node can be a parent of this specific node
+	 * 
+	 * @param node
+	 *            To be tested node
+	 * @return true if valid
+	 */
+	public final boolean isValidParentOf(Node node) {
+		return validChildClasses().contains(node.getClass());
+	}
+
+	/**
+	 * Parent of the node
+	 */
 	protected Node parent = null;
+	/**
+	 * Contains the list of children
+	 */
 	protected List<Node> childs = new Vector<Node>();
 
-	public List<Node> getChilds() {return Collections.unmodifiableList(childs);}
+	/**
+	 * Returns the List of children
+	 * 
+	 * @return List of Children
+	 */
+	public List<Node> getChilds() {
+		return Collections.unmodifiableList(childs);
+	}
 
-	public enum Acceptance
-	{
-		OK
-		,INVALID_PARENT,ALREADY_HAS_A_PARENT,CANNOT_ACCEPT_ANYMORE_CHILDREN,SOURCE_PROPERTY_EXPECTED,TARGET_PROPERTY_EXPECTED}
+	/**
+	 * Enum of Acceptance values
+	 */
+	public enum Acceptance {
+		OK, INVALID_PARENT, ALREADY_HAS_A_PARENT, CANNOT_ACCEPT_ANYMORE_CHILDREN, SOURCE_PROPERTY_EXPECTED, TARGET_PROPERTY_EXPECTED
+	}
 
-	public boolean addChild(Node child)
-	{
-		if(!acceptsChild(child)) {return false;}
+	/**
+	 * Adds a Child to the Node
+	 * 
+	 * @param child
+	 *            Child to be added
+	 * @return True if successful
+	 */
+	public boolean addChild(Node child) {
+		if (!acceptsChild(child)) {
+			return false;
+		}
 		childs.add(child);
-		child.parent=this;
+		child.parent = this;
 		child.pushDownColor(this.color);
 		return true;
 	}
 
-	public void removeChild(Node child)
-	{
+	/**
+	 * Removes child from node
+	 * 
+	 * @param child
+	 *            to be removed
+	 */
+	public void removeChild(Node child) {
 		childs.remove(child);
-		child.parent=null;
+		child.parent = null;
 		child.pushDownColor(colors++);
 	}
 
-	public boolean acceptsChild(Node n) {return isValidParentOf(n)&&n.parent==null&&childs.size()<getMaxChilds()&&this.color!=n.color;}
+	/**
+	 * Returns true if Child will be accepted
+	 * 
+	 * @param n
+	 *            node to test
+	 * @return true if Child is accepted
+	 */
+	public boolean acceptsChild(Node n) {
+		return isValidParentOf(n) && n.parent == null
+				&& childs.size() < getMaxChilds() && this.color != n.color;
+	}
 
-	public Acceptance acceptsChildWithReason(Node n)
-	{
-		if(!isValidParentOf(n)) {return Acceptance.INVALID_PARENT;}
-		if(n.parent!=null) {return Acceptance.ALREADY_HAS_A_PARENT;}
-		if(childs.size()>=getMaxChilds()) {return Acceptance.CANNOT_ACCEPT_ANYMORE_CHILDREN;}
+	/**
+	 * Gives the Reason why Child is not accepted
+	 * 
+	 * @param n
+	 *            node to be tested
+	 * @return Reason why it is not accepted
+	 */
+	public Acceptance acceptsChildWithReason(Node n) {
+		if (!isValidParentOf(n)) {
+			return Acceptance.INVALID_PARENT;
+		}
+		if (n.parent != null) {
+			return Acceptance.ALREADY_HAS_A_PARENT;
+		}
+		if (childs.size() >= getMaxChilds()) {
+			return Acceptance.CANNOT_ACCEPT_ANYMORE_CHILDREN;
+		}
 		return Acceptance.OK;
 	}
 
-	public boolean isComplete()
-	{
-		if(childs.size()<getMaxChilds()) {return false;}
-		for(Node child : childs) {if(!child.isComplete()) return false;}
+	/**
+	 * Tests if Node has all needed Children
+	 * 
+	 * @return True if node has maximum childs
+	 */
+	public boolean isComplete() {
+		if (childs.size() < getMaxChilds()) {
+			return false;
+		}
+		for (Node child : childs) {
+			if (!child.isComplete())
+				return false;
+		}
 		return true;
 	}
 
-	public Node(String id)
-	{
-		this.id=id;
+	/**
+	 * Constructor
+	 * 
+	 * @param id
+	 *            Identifier of the Node
+	 */
+	public Node(String id) {
+		this.id = id;
 		color = colors++;
 	}
 
-	protected void pushDownColor(int color)
-	{
+	/**
+	 * Changes Color of Node and children
+	 * 
+	 * @param color
+	 */
+	protected void pushDownColor(int color) {
 		this.color = color;
-		for(Node child : childs) {child.pushDownColor(color);}
+		for (Node child : childs) {
+			child.pushDownColor(color);
+		}
 	}
 
-	@Override 
-	public String toString()
-	{
+	/**
+	 * Builds MetricExpressionString
+	 */
+	@Override
+	public String toString() {
 		StringBuilder sb = new StringBuilder(id);
-		synchronized(this)
-		{
-			if(getChilds().isEmpty()) {return sb.toString();}
-			int i=0;
-			for(Node child: getChilds())
-			{
-				if(i==0) {sb.append('(');} else {sb.append(',');}
-				if(this.hasFrontParameters())
-				{
+		synchronized (this) {
+			if (getChilds().isEmpty()) {
+				return sb.toString();
+			}
+			int i = 0;
+			for (Node child : getChilds()) {
+				if (i == 0) {
+					sb.append('(');
+				} else {
+					sb.append(',');
+				}
+				if (this.hasFrontParameters()) {
 					Double p;
-					if((p=(i==0?param1:param2))!=null) 	{sb.append(p);sb.append('*');}
+					if ((p = (i == 0 ? param1 : param2)) != null) {
+						sb.append(p);
+						sb.append('*');
+					}
 				}
 				sb.append(child.toString());
 
-				if(!this.hasFrontParameters())
-				{
+				if (!this.hasFrontParameters()) {
 					Double p;
-					if((p=(i==0?param1:param2))!=null) 	{sb.append('|');sb.append(p);}
+					if ((p = (i == 0 ? param1 : param2)) != null) {
+						sb.append('|');
+						sb.append(p);
+					}
 				}
 				i++;
 			}
@@ -100,49 +224,79 @@ public abstract class Node
 		sb.append(')');
 		return sb.toString();
 	}
-	public static Node createNode(String id)
-	{
+
+	/**
+	 * Create new Node
+	 * 
+	 * @param id
+	 *            Identifier of new Node
+	 * @return New Node
+	 */
+	public static Node createNode(String id) {
 		id = id.toLowerCase();
-		if(Measure.identifiers.contains(id)) return new Measure(id);
-		if(Operator.identifiers.contains(id)) return new Operator(id);
-		throw new MetricFormatException("There is no node with id \""+id+"\", creation is not possible.");
+		if (Measure.identifiers.contains(id))
+			return new Measure(id);
+		if (Operator.identifiers.contains(id))
+			return new Operator(id);
+		throw new MetricFormatException("There is no node with id \"" + id
+				+ "\", creation is not possible.");
 	}
 
-	public boolean hasFrontParameters() {return "add".equalsIgnoreCase(id);}
+	/**
+	 * Returns true if node has FrontParameters
+	 * 
+	 * @return true if equals add = true
+	 */
+	public boolean hasFrontParameters() {
+		return "add".equalsIgnoreCase(id);
+	}
 
+	/**
+	 * Compares Object and this Node
+	 * 
+	 * @return true if they are the same
+	 */
 	public boolean equals(Object o) {
 		return equals(o, false);
 	}
 
+	/**
+	 * Test if Object and Node are the same
+	 * 
+	 * @param o
+	 *            Tested Object
+	 * @param strict
+	 *            If True test String and Parents
+	 * @return Return True if they are equal
+	 */
 	@SuppressWarnings("unused")
-	public boolean equals(Object o, boolean strict)
-	{
-		if(o==null||!(this.getClass()==o.getClass()))
+	public boolean equals(Object o, boolean strict) {
+		if (o == null || !(this.getClass() == o.getClass()))
 			return false;
 		Node n2 = (Node) o;
 		boolean stringTest = false, parentTest = true, childTest = true, completeTest;
 		stringTest = this.toString().equals(o.toString());
-		if((this.parent == null && n2.parent != null) || (n2.parent==null && this.parent != null)) {
-		}
-		else if(this.parent != null && n2.parent != null)
-			if(this.parent == n2.parent) { // same parent
+		if ((this.parent == null && n2.parent != null)
+				|| (n2.parent == null && this.parent != null)) {
+		} else if (this.parent != null && n2.parent != null)
+			if (this.parent == n2.parent) { // same parent
 
 			} else {
 				parentTest = false;
 			}
-		if(this.getMaxChilds() == n2.getMaxChilds() && this.getMaxChilds()>0) {
+		if (this.getMaxChilds() == n2.getMaxChilds() && this.getMaxChilds() > 0) {
 			List<Node> childrens1 = this.getChilds();
 			List<Node> childrens2 = n2.getChilds();
-			for(int i = 0; i<childrens1.size(); i++) {
-				for(int j = 0; j < childrens2.size(); j++)
-					if(!childrens1.get(i).equals(childrens2.get(j)))
-							childTest = false;
+			for (int i = 0; i < childrens1.size(); i++) {
+				for (int j = 0; j < childrens2.size(); j++)
+					if (!childrens1.get(i).equals(childrens2.get(j)))
+						childTest = false;
 			}
-		}else {
+		} else {
 			childTest = false;
 		}
 		completeTest = this.isComplete() == n2.isComplete();
-		if(strict)
+		if (strict)
 			return stringTest && parentTest;
 		else
 			return stringTest;
